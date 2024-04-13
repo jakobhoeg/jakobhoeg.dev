07112269a37339a51bb59900a53df0c3eb37580f
Jakob Hoeg Mørk <jakmoe01@easv365.dk>
2024-04-13 21:48:07 +0200

    feat: final mobile layout homepage

--- src/components/cards/linkedin.tsx
+++ src/components/cards/linkedin.tsx
@@ -11,7 +11,7 @@ export default function LinkedInCard() {
   return (
    <motion.div
     whileHover={{ scale: 1.025 }}
-   className='relative h-full gap-6 overflow-hidden rounded-xl  p-4 col-start-3 col-span-2 row-start-2  ring-1 ring-neutral-200 ring-offset-8 ring-offset-neutral-100 dark:ring-neutral-950/10 dark:ring-offset-neutral-950/10'>
+   className='relative h-full w-full gap-6 overflow-hidden rounded-xl  p-4 col-start-3 col-span-2 row-start-2  ring-1 ring-neutral-200 ring-offset-8 ring-offset-neutral-100 dark:ring-neutral-950/10 dark:ring-offset-neutral-950/10'>
      <Link href="https://www.linkedin.com/in/jakob-hoeg-moerk/"
     className={cn(buttonVariants({ variant: "linkedin" }), 
     'absolute top-0 left-0 h-full w-full gap-2')}
