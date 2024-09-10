13eb97c6b784a8406c3769ca53a3c615aba1186e
Jakob Hoeg <jakmoe01@easv365.dk>
2024-09-10 20:02:00 +0200

    use shadcn-chat

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -59,6 +59,22 @@ export const Projects: ProjectCardProps[] = [
   },
   {
     id: 2,
+    title: 'shadcn-chat',
+    description:
+      'CLI for adding customizable and re-usable chat components to your react applications.',
+    videoUrl:
+      'https://utfs.io/f/3c7ba35d-d065-4e10-ac80-54bb7bd1f6bb-62d9he.MP4',
+    videoAlt: 'shadcn-chat',
+    showcaseUrl: 'https://shadcn-chat.vercel.app/',
+    githubUrl: 'https://github.com/jakobhoeg/shadcn-chat',
+    skills: [
+      '/assets/icons/nextdotjs.svg',
+      '/assets/icons/typescript.svg',
+      '/assets/icons/tailwindcss.svg',
+    ],
+  },
+  {
+    id: 3,
     title: 'Proglio',
     description:
       'A full-stack collaborative task management platform coded in React with NextJS, MongoDB & TypeScript.',
@@ -74,7 +90,7 @@ export const Projects: ProjectCardProps[] = [
     ],
   },
   {
-    id: 3,
+    id: 4,
     title: 'Web app for Ollama LLMs',
     description:
       'Open source web interface for Ollama. Run open-source large language models (LLMs) locally on your pc. The web interface is coded in React with NextJS & TypeScript.',
@@ -82,22 +98,6 @@ export const Projects: ProjectCardProps[] = [
       'https://utfs.io/f/afb47c2a-6f2d-4b45-8e16-d9cd484b3221-grr6xm.MP4',
     videoAlt: 'Web UI for Ollama LLMs',
     githubUrl: 'https://github.com/jakobhoeg/nextjs-ollama-llm-ui',
-    skills: [
-      '/assets/icons/nextdotjs.svg',
-      '/assets/icons/typescript.svg',
-      '/assets/icons/tailwindcss.svg',
-    ],
-  },
-  {
-    id: 4,
-    title: 'shadcn-chat',
-    description:
-      'Open source chat components for NextJS. Built on top of the very popular UI library, shadcn.',
-    videoUrl:
-      'https://utfs.io/f/3c7ba35d-d065-4e10-ac80-54bb7bd1f6bb-62d9he.MP4',
-    videoAlt: 'Moodle Lazy DL Bot',
-    showcaseUrl: 'https://shadcn-chat.vercel.app/',
-    githubUrl: 'https://github.com/jakobhoeg/shadcn-chat',
     skills: [
       '/assets/icons/nextdotjs.svg',
       '/assets/icons/typescript.svg',
